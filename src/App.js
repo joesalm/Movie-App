f5896a0fcda1c57952daca1cd35a90c4b4236357
Yossef <joesalm@hotmail.com>
2020-08-03 23:18:30 +0300

    before age

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ActorModel from './data-model/ActorModel'
+
+
 
 function App() {
+
+  var arrOfActors=[];
+  
+  const actor1 = new ActorModel("Harrison", "Ford", "1942-07-13", "https://www.imdb.com/name/nm0000148/", "https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_.jpg");
+  const actor2 = new ActorModel("Connelly", "Jennifer", "1970-12-12", "https://www.imdb.com/name/nm0000124/?ref_=nv_sr_srsg_0", "https://m.media-amazon.com/images/M/MV5BOTczNTgzODYyMF5BMl5BanBnXkFtZTcwNjk4ODk4Mw@@._V1_.jpg");
+  
+  arrOfActors = [actor1, actor2];
+  
+  console.log(arrOfActors);
+  // console.log(actor1);
+  const fnames = [];
+  
+  const renderActor = () => {
+
+    arrOfActors.forEach(element => {
+      console.log(element); // printing the actors / actress to the log with all the details for each and every one of them.
+      // console.log(element.fname);
+      fnames.push(element.fname); // just for fun it uses to print only the first names of the actors or actresses.
+      // console.log(fnames.length);
+    });
+    // for(let i in actor1) {
+      // console.log(i);
+      
+      // console.log(actor1);
+      // fnames.push (actor1[fname]);
+      // console.log(i);
+    //   fnames.push(actor1[i]);
+    // }
+    // return fnames.length;
+    // return fname;
+  }
+
+  renderActor();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {fnames + ' '}
+      </div>
     </div>
   );
 }
--- src/data-model/ActorModel.js
+++ src/data-model/ActorModel.js
@@ -1,13 +1,15 @@
-class Actor {
+class ActorModel {
     constructor (fname, lname, birthdate, image, imdbLink) {
         this.fname = fname;
         this.lname = lname;
         this.birthdate = birthdate;
         this.image = image;
-
+        this.imdbLink  = imdbLink;
     }
 
     Age () {
         return 10;
     }
 }
+
+export default ActorModel;
