74549ecd4838c4568504f13520b2f4d9b126548b
Yossef <joesalm@hotmail.com>
2020-08-03 14:11:45 +0300

    defining the Actor class

---
+++ src/data-model/Actor.js
@@ -0,0 +1,13 @@
+class Actor {
+    constructor (fname, lname, birthdate, image, imdbLink) {
+        this.fname = fname;
+        this.lname = lname;
+        this.birthdate = birthdate;
+        this.image = image;
+
+    }
+
+    Age () {
+        return 10;
+    }
+}
