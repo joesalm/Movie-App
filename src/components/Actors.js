99d0addbeb1d7a7872781c96979f3d8fdd84347c
Yossef <joesalm@hotmail.com>
2020-08-05 00:13:20 +0300

    the application is now fully responsive and also i just declared the edit box for searching an actor/actress. but it's yet implmenented.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -11,21 +11,35 @@ class Actors extends React.Component {
   }
 
   render() {
+
     const arrOfActors = this.props.arrOfActors;
 
     const cards = arrOfActors.map((currentActor) => {
       return <div >
-                <Col
-                //  xs={12} sm={6} md={4} lg={3}
-                >
-                  <Actor CurrentActor={currentActor} key={currentActor} /> 
-                </Col>
-             </div>
-   });
+        <Col
+        //  xs={12} sm={6} md={4} lg={3}
+        >
+          <Actor CurrentActor={currentActor} key={currentActor} />
+        </Col>
+      </div>
+    });
 
     return (
-
       <div>
+
+        <div id="form">
+          <form>
+            <fieldset>
+              <label for="Search"> Search by name: </label>
+              <input if="search-btn" />
+
+              <button name="search-btn" type="button" value="Search" />
+            </fieldset>
+          </form>
+        </div>
+
+
+
         <Container fluid>
           <Row>
             {cards}
