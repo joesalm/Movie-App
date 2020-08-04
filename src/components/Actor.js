1b849c1c4a485159fff2a870a6b4f1d9b7c2abc7
Yossef <joesalm@hotmail.com>
2020-08-04 21:58:32 +0300

    trying to find out how to use map function as respnonsive rendering...

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class Actor extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  
-  
-  render() {
-    const currActor = this.props.CurrentActor;
-    return (
-      <div>{currActor.fname}</div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const currActor = this.props.CurrentActor;
+        return (
+
+
+
+
+            <div id="Actor-Card">
+                <div className="table-responsive">
+                    <img id="Actor-Pic" src={currActor.image} />
+                    {/* {currActor.image} */}
+                </div>
+                <a id="Actor-Name" href={currActor.imdbLink} target="_blank"> {currActor.fname + ' ' + currActor.lname} </a>
+                <p>Age: {currActor.Age()}</p>
+            </div>
+        );
+    }
 }
 
 export default Actor;
--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// import {Container} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+// import { Container, Row, Col } from 'reactstrap';
 import ReactDOM from 'react-dom';
 import Actor from './Actor';
 
@@ -11,17 +14,57 @@ class Actors extends React.Component {
     const arrOfActors = this.props.arrOfActors;
 
     return (
-      <div>
-        {/* {arrOfActors[0].fname} */ }
-        {/* <Actor/> */ }
 
-        {arrOfActors.map((currentActor) =>
-          <div key={currentActor}> <Actor CurrentActor={currentActor}/> </div>)
-        }
-      </div>)
+      <div>
+        <Container>
+
+          <Row>
+            <Col sm="12" md="6" lg="4">
+
+                {/* {arrOfActors[0].fname} */}
+                {/* <Actor/> */}
+                {arrOfActors.map((currentActor) =>
+                  <div > <Actor CurrentActor={currentActor} /> </div>)
+                }
+
+            </Col>
+            <Col sm="12" md="6" lg="4">
+                {/* {arrOfActors[0].fname} */}
+                {/* <Actor/> */}
+                {arrOfActors.map((currentActor) =>
+                  <div > <Actor CurrentActor={currentActor} /> </div>)
+                }
+
+            </Col>
+            <Col sm="12" md="6" lg="4">
+                {/* {arrOfActors[0].fname} */}
+                {/* <Actor/> */}
+                {arrOfActors.map((currentActor) =>
+                  <div > <Actor CurrentActor={currentActor} /> </div>)
+                }
+
+            </Col>
+          </Row>
+
+        </Container>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    )
   }
 }
 
 export default Actors;
-
-
