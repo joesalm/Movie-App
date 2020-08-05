283ca8831ce7e76dbef50e97a2a46bb0fea8df0b
Yossef <joesalm@hotmail.com>
2020-08-06 02:26:29 +0300

    switched between the first name and last names.

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
 
   var arrOfActors=[];
   
-  const actor1 = new ActorModel("Ford", "Harrison", "1942-07-13", "https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_.jpg","https://www.imdb.com/name/nm0000148/");
-  const actor2 = new ActorModel("Connelly", "Jennifer", "1970-12-12", "https://m.media-amazon.com/images/M/MV5BOTczNTgzODYyMF5BMl5BanBnXkFtZTcwNjk4ODk4Mw@@._V1_.jpg", "https://www.imdb.com/name/nm0000124/?ref_=nv_sr_srsg_0");
-  const actor3 = new ActorModel("Guinness", "Alec", "1914-04-02", "https://m.media-amazon.com/images/M/MV5BMTIxMTA5OTI2M15BMl5BanBnXkFtZTYwNjEwNjU2._V1_.jpg", "https://www.imdb.com/name/nm0000027/?ref_=nv_sr_srsg_0");
-  const actor4 = new ActorModel("Reeve", "Christopher", "1952-09-25", "https://m.media-amazon.com/images/M/MV5BMjE1MDYwNjQxMF5BMl5BanBnXkFtZTcwNDE4NzU3MQ@@._V1_.jpg","https://www.imdb.com/name/nm0001659/?ref_=fn_al_nm_1");
-  const actor5 = new ActorModel("Kaye", "Danny", "1911-01-18", "https://m.media-amazon.com/images/M/MV5BMTUwNDE2OTYyN15BMl5BanBnXkFtZTYwMzEzMzQ2._V1_.jpg", "https://www.imdb.com/name/nm0001414/?ref_=nv_sr_srsg_0");
-  const actor6 = new ActorModel("Earl Jones", "James", "1931-01-17", "https://m.media-amazon.com/images/M/MV5BMTcwNTg1MzMwM15BMl5BanBnXkFtZTcwNTQyMDgyMg@@._V1_.jpg", "https://www.imdb.com/name/nm0000469/?ref_=nv_sr_srsg_0");
+  const actor1 = new ActorModel("Harrison", "Ford", "1942-07-13", "https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_.jpg","https://www.imdb.com/name/nm0000148/");
+  const actor2 = new ActorModel("Jennifer", "Connelly", "1970-12-12", "https://m.media-amazon.com/images/M/MV5BOTczNTgzODYyMF5BMl5BanBnXkFtZTcwNjk4ODk4Mw@@._V1_.jpg", "https://www.imdb.com/name/nm0000124/?ref_=nv_sr_srsg_0");
+  const actor3 = new ActorModel("Alec", "Guinness", "1914-04-02", "https://m.media-amazon.com/images/M/MV5BMTIxMTA5OTI2M15BMl5BanBnXkFtZTYwNjEwNjU2._V1_.jpg", "https://www.imdb.com/name/nm0000027/?ref_=nv_sr_srsg_0");
+  const actor4 = new ActorModel("Christopher", "Reeve", "1952-09-25", "https://m.media-amazon.com/images/M/MV5BMjE1MDYwNjQxMF5BMl5BanBnXkFtZTcwNDE4NzU3MQ@@._V1_.jpg","https://www.imdb.com/name/nm0001659/?ref_=fn_al_nm_1");
+  const actor5 = new ActorModel("Danny", "Kaye", "1911-01-18", "https://m.media-amazon.com/images/M/MV5BMTUwNDE2OTYyN15BMl5BanBnXkFtZTYwMzEzMzQ2._V1_.jpg", "https://www.imdb.com/name/nm0001414/?ref_=nv_sr_srsg_0");
+  const actor6 = new ActorModel("James", "Earl Jones", "1931-01-17", "https://m.media-amazon.com/images/M/MV5BMTcwNTg1MzMwM15BMl5BanBnXkFtZTcwNTQyMDgyMg@@._V1_.jpg", "https://www.imdb.com/name/nm0000469/?ref_=nv_sr_srsg_0");
 
   // const getAge = require('age-by-birthdate');
   // console.log(getAge('1/17/1972'));
